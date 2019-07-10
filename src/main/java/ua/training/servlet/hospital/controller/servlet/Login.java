eada4f144992182e1df4e06caab79007e46b4b60
Shchava <yarosshc@gmail.com>
2019-07-11 00:40:30 +0300

    Initial commit

---
+++ src/main/java/ua/training/servlet/hospital/controller/servlet/Login.java
@@ -0,0 +1,9 @@
+package ua.training.servlet.hospital.controller.servlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet("/login/")
+public class Login extends HttpServlet {
+
+}
