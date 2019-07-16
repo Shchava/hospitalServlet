70f45c3cfa23ce33c8de7e0e9db1d367c848c734
Shchava <yarosshc@gmail.com>
2019-07-17 00:37:26 +0300

    add test database schema and data declarations

---
+++ src/test/resources/hospitalDatabaseData.sql
@@ -0,0 +1,12 @@
+INSERT INTO USER(name, surname, patronymic, email, password_hash, role)
+VALUES ('userName1', 'userSurname1', 'userPatronymic1', 'email1@example.com', 'someHash1', 'DOCTOR');
+INSERT INTO USER(name, surname, patronymic, email, password_hash, role)
+VALUES ('userName2', 'userSurname2', 'userPatronymic2', 'email2@example.com', 'someHash2', 'NURSE');
+INSERT INTO USER(name, surname, patronymic, email, password_hash, role)
+VALUES ('userName3', 'userSurname3', 'userPatronymic3', 'email3@example.com', 'someHash3', 'PATIENT');
+INSERT INTO USER(name, surname, patronymic, email, password_hash, role)
+VALUES ('userName4', 'userSurname4', 'userPatronymic4', 'email4@example.com', 'someHash4', 'DOCTOR');
+INSERT INTO USER(name, surname, patronymic, email, password_hash, role)
+VALUES ('userName5', 'userSurname5', 'userPatronymic5', 'email5@example.com', 'someHash5', 'NURSE');
+INSERT INTO USER(name, surname, patronymic, email, password_hash, role)
+VALUES ('userName6', 'userSurname6', 'userPatronymic6', 'email6@example.com', 'someHash6', 'PATIENT');
