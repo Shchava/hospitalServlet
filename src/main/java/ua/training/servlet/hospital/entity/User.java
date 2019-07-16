0e54605abc10e00a2004eddb4300748a4da77be2
Shchava <yarosshc@gmail.com>
2019-07-17 00:35:42 +0300

    add patronymic field

--- src/main/java/ua/training/servlet/hospital/entity/User.java
+++ src/main/java/ua/training/servlet/hospital/entity/User.java
@@ -8,6 +8,7 @@ public class User {
     private long id;
     private String name;
     private String surname;
+    private String patronymic;
     private String email;
     private String passwordHash;
     private Roles role;
@@ -16,16 +17,17 @@ public class User {
     public User() {
     }
 
-    public User(String name, String surname, String email, String passwordHash, Roles role) {
+    public User(String name, String surname, String patronymic, String email, String passwordHash, Roles role) {
         this.name = name;
         this.surname = surname;
+        this.patronymic = patronymic;
         this.email = email;
         this.passwordHash = passwordHash;
         this.role = role;
     }
 
-    public User(long id, String name, String surname, String email, String passwordHash, Roles role) {
-        this(name, surname, email, passwordHash, role);
+    public User(long id, String name, String surname, String patronymic, String email, String passwordHash, Roles role) {
+        this(name, surname, patronymic,email, passwordHash, role);
         this.id = id;
     }
 
@@ -39,6 +41,10 @@ public class User {
 
     public String getSurname() {
         return surname;
+    }
+
+    public String getPatronymic() {
+        return patronymic;
     }
 
     public String getEmail() {
@@ -65,6 +71,10 @@ public class User {
         this.surname = surname;
     }
 
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
+    }
+
     public void setEmail(String email) {
         this.email = email;
     }
@@ -75,6 +85,38 @@ public class User {
 
     public void setRole(Roles role) {
         this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", patronymic='" + patronymic + '\'' +
+                ", email='" + email + '\'' +
+                ", passwordHash='" + passwordHash + '\'' +
+                ", role=" + role +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id &&
+                Objects.equals(name, user.name) &&
+                Objects.equals(surname, user.surname) &&
+                Objects.equals(patronymic, user.patronymic) &&
+                Objects.equals(email, user.email) &&
+                Objects.equals(passwordHash, user.passwordHash) &&
+                role == user.role;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, surname, patronymic, email, passwordHash, role);
     }
 }
 
