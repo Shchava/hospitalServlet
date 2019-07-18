109de4a83d929fb0780db7e07eba4dcdae5c8870
Shchava <yarosshc@gmail.com>
2019-07-18 20:26:20 +0300

    make JDBCMedicineDao and JDBCUserDao extended from JDBCGenericDao and using its as implementation of querying

--- src/main/java/ua/training/servlet/hospital/dao/impl/JDBCMedicineDao.java
+++ src/main/java/ua/training/servlet/hospital/dao/impl/JDBCMedicineDao.java
@@ -2,148 +2,38 @@ package ua.training.servlet.hospital.dao.impl;
 
 import ua.training.servlet.hospital.dao.MedicineDao;
 import ua.training.servlet.hospital.dao.mapper.MedicineMapper;
-import ua.training.servlet.hospital.dao.mapper.ObjectMapper;
-import ua.training.servlet.hospital.dao.mapper.UserMapper;
 import ua.training.servlet.hospital.entity.Medicine;
-import ua.training.servlet.hospital.entity.User;
 
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
 
-public class JDBCMedicineDao implements MedicineDao {
-    private Connection connection;
-
+public class JDBCMedicineDao extends JDBCGenericDao<Medicine> implements MedicineDao {
     public JDBCMedicineDao(Connection connection) {
-        this.connection = connection;
+        super(
+                connection,
+                "INSERT INTO medicine(name, description, assigned, assigned_by_id_user, count, refill) VALUES(?,?,?,?,?,?)",
+                "SELECT * FROM medicine LEFT JOIN user ON(medicine.assigned_by_id_user = user.id_user) WHERE id_therapy = ?",
+                "SELECT SQL_CALC_FOUND_ROWS * FROM medicine LEFT JOIN user ON(medicine.assigned_by_id_user = user.id_user) LIMIT ?,?",
+                "SELECT * FROM medicine LEFT JOIN user ON(medicine.assigned_by_id_user = user.id_user)",
+                "SELECT COUNT(*)FROM medicine",
+                "COUNT(*)",
+                "UPDATE medicine SET name = ?, description = ?, assigned = ?, assigned_by_id_user = ?, count = ?, refill = ? WHERE id_therapy = ?",
+                7,
+                "DELETE FROM medicine WHERE id_therapy = ?",
+                new MedicineMapper());
     }
 
     @Override
-    public boolean create(Medicine entity) {
-        boolean created = false;
-        final String query = "INSERT INTO medicine(name, description, assigned, assigned_by_id_user, count, refill) VALUES(?,?,?,?,?,?)";
-        try(PreparedStatement statement =  connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS)){
-            setEntityValues(statement,entity);
-
-            int affected = statement.executeUpdate();
-            if(affected == 1){
-                getId(entity,statement);
-                created = true;
-            }
-        }catch (Exception ex){
-            ex.printStackTrace();
-        }
-        return created;
+    long getId(Medicine entity) {
+        return entity.getId();
     }
 
     @Override
-    public Optional<Medicine> findById(long id) {
-        Medicine entity = null;
-        ObjectMapper<Medicine> mapper = new MedicineMapper();
-
-        final String query = "SELECT * FROM medicine LEFT JOIN user ON(medicine.assigned_by_id_user = user.id_user) WHERE id_therapy = " + id +";";
-        try(Statement statement =  connection.createStatement()){
-            ResultSet result = statement.executeQuery(query);
-            if(result.next()){
-                entity = mapper.extractFromResultSet(result);
-            }
-        }catch (Exception ex){
-            ex.printStackTrace();
-        }
-        return Optional.ofNullable(entity);
+    void setId(Medicine entity, long Id) throws SQLException {
+        entity.setId(Id);
     }
 
     @Override
-    public List<Medicine> findRange(int start, int count) {
-        final String query = "SELECT SQL_CALC_FOUND_ROWS * FROM medicine LEFT JOIN user ON(medicine.assigned_by_id_user = user.id_user) LIMIT " + start + "," + count;
-        return getAllFromQuery(query);
-    }
-
-    @Override
-    public List<Medicine> findAll() {
-        final String query = "SELECT * FROM medicine LEFT JOIN user ON(medicine.assigned_by_id_user = user.id_user)";
-        return getAllFromQuery(query);
-    }
-
-
-    @Override
-    public int count() {
-        int count = 0;
-        final String query = "SELECT COUNT(*)FROM medicine";
-        try(Statement statement = connection.createStatement()){
-            ResultSet rs = statement.executeQuery(query);
-            if(rs.next()){
-                count = rs.getInt("COUNT(*)");
-            }
-        }catch (Exception ex){
-            ex.printStackTrace();
-        }
-        return count;
-    }
-
-    @Override
-    public boolean update(Medicine entity) {
-        boolean created = false;
-        final String query = "UPDATE medicine SET name = ?, description = ?, assigned = ?, assigned_by_id_user = ?, count = ?, refill = ? WHERE id_therapy = ?";
-        try(PreparedStatement statement =  connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS)){
-            setEntityValues(statement,entity);
-            statement.setLong(7,entity.getId());
-
-            int affected = statement.executeUpdate();
-            created = affected == 1;
-        }catch (Exception ex){
-            ex.printStackTrace();
-        }
-        return created;
-    }
-
-    @Override
-    public boolean delete(long id) {
-        boolean affected = false;
-        final String query = "DELETE FROM medicine WHERE id_therapy = " + id + ";";
-        try(Statement statement =  connection.createStatement()){
-            statement.execute(query);
-            affected = true;
-        }catch (Exception ex){
-            ex.printStackTrace();
-        }
-        return affected;
-    }
-
-    @Override
-    public void close() {
-        try {
-            connection.close();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private void getId(Medicine medicine, Statement statement) throws SQLException {
-        ResultSet generatedKeys = statement.getGeneratedKeys();
-        if (generatedKeys.next()) {
-            medicine.setId(generatedKeys.getLong(1));
-        }
-    }
-
-    private List<Medicine> getAllFromQuery(String query){
-        List<Medicine> entities = new ArrayList<>();
-        ObjectMapper<Medicine> mapper = new MedicineMapper();
-        try (Statement st = connection.createStatement()) {
-            ResultSet rs = st.executeQuery(query);
-            while (rs.next()) {
-                Medicine entity = mapper.extractFromResultSet(rs);
-                entities.add(entity);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return entities;
-        }
-        return entities;
-    }
-
-    private void setEntityValues(PreparedStatement statement, Medicine entity) throws SQLException{
+    void setEntityValues(PreparedStatement statement, Medicine entity) throws SQLException{
         statement.setString(1,entity.getName());
         statement.setString(2,entity.getDescription());
         statement.setTimestamp(3,Timestamp.valueOf(entity.getAssigned()));
--- src/main/java/ua/training/servlet/hospital/dao/impl/JDBCUserDao.java
+++ src/main/java/ua/training/servlet/hospital/dao/impl/JDBCUserDao.java
@@ -9,138 +9,34 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-public class JDBCUserDao implements UserDao {
-    private Connection connection;
-
-    JDBCUserDao(Connection connection) {
-        this.connection = connection;
+public class JDBCUserDao extends JDBCGenericDao<User> implements UserDao {
+    public JDBCUserDao(Connection connection) {
+        super(
+                connection,
+                "INSERT INTO user(name, surname, patronymic, email, password_hash, role) VALUES(?,?,?,?,?,?)",
+                "SELECT * FROM user WHERE id_user = ?",
+                "SELECT SQL_CALC_FOUND_ROWS * FROM user LIMIT ?,?",
+                "SELECT * FROM user",
+                "SELECT COUNT(*)FROM user",
+                "COUNT(*)",
+                "UPDATE user SET name = ?, surname = ?, patronymic = ?, email = ?, password_hash = ?, role = ? WHERE id_user = ?",
+                7,
+                "DELETE FROM user WHERE id_user = ?",
+                new UserMapper());
     }
 
     @Override
-    public boolean create(User entity) {
-        boolean created = false;
-        final String query = "INSERT INTO user(name, surname, patronymic, email, password_hash, role) VALUES(?,?,?,?,?,?)";
-        try(PreparedStatement statement =  connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS)){
-            setEntityValues(statement,entity);
-
-            int affected = statement.executeUpdate();
-            if(affected == 1){
-                getId(entity,statement);
-                created = true;
-            }
-        }catch (Exception ex){
-            ex.printStackTrace();
-        }
-        return created;
+    long getId(User entity) {
+        return entity.getId();
     }
 
     @Override
-    public Optional<User> findById(long id) {
-        User user = null;
-        UserMapper mapper = new UserMapper();
-
-        final String query = "SELECT * FROM user WHERE id_user = " + id +";";
-        try(Statement statement =  connection.createStatement()){
-            ResultSet result = statement.executeQuery(query);
-            if(result.next()){
-                user = mapper.extractFromResultSet(result);
-            }
-        }catch (Exception ex){
-            ex.printStackTrace();
-        }
-        return Optional.ofNullable(user);
+    void setId(User entity, long Id) throws SQLException {
+        entity.setId(Id);
     }
 
     @Override
-    public List<User> findRange(int start, int count) {
-        final String query = "SELECT SQL_CALC_FOUND_ROWS * FROM user LIMIT " + start + "," + count;
-        return getAllFromQuery(query);
-    }
-
-    @Override
-    public List<User> findAll() {
-        final String query = "SELECT * FROM user";
-        return getAllFromQuery(query);
-    }
-
-
-    @Override
-    public int count() {
-        int count = 0;
-        final String query = "SELECT COUNT(*)FROM user";
-        try(Statement statement = connection.createStatement()){
-            ResultSet rs = statement.executeQuery(query);
-            if(rs.next()){
-                count = rs.getInt("COUNT(*)");
-            }
-        }catch (Exception ex){
-            ex.printStackTrace();
-        }
-        return count;
-    }
-
-    @Override
-    public boolean update(User entity) {
-        boolean created = false;
-        final String query = "UPDATE user SET name = ?, surname = ?, patronymic = ?, email = ?, password_hash = ?, role = ? WHERE id_user = ?";
-        try(PreparedStatement statement =  connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS)){
-            setEntityValues(statement,entity);
-            statement.setLong(7,entity.getId());
-
-            int affected = statement.executeUpdate();
-            created = affected == 1;
-        }catch (Exception ex){
-            ex.printStackTrace();
-        }
-        return created;
-    }
-
-    @Override
-    public boolean delete(long id) {
-        boolean affected = false;
-        final String query = "DELETE FROM user WHERE id_user = " + id + ";";
-        try(Statement statement =  connection.createStatement()){
-            statement.execute(query);
-            affected = true;
-        }catch (Exception ex){
-            ex.printStackTrace();
-        }
-        return affected;
-    }
-
-    @Override
-    public void close() {
-        try {
-            connection.close();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private void getId(User user, Statement statement) throws SQLException {
-        ResultSet generatedKeys = statement.getGeneratedKeys();
-        if (generatedKeys.next()) {
-            user.setId(generatedKeys.getLong(1));
-        }
-    }
-
-    private List<User> getAllFromQuery(String query){
-        List<User> users = new ArrayList<>();
-        UserMapper mapper = new UserMapper();
-        try (Statement st = connection.createStatement()) {
-            ResultSet rs = st.executeQuery(query);
-            while (rs.next()) {
-                User user = mapper.extractFromResultSet(rs);
-                users.add(user);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return users;
-        }
-        return users;
-    }
-
-    private void setEntityValues(PreparedStatement statement, User entity) throws SQLException{
+    void setEntityValues(PreparedStatement statement, User entity) throws SQLException{
         statement.setString(1,entity.getName());
         statement.setString(2,entity.getSurname());
         statement.setString(3,entity.getPatronymic());
