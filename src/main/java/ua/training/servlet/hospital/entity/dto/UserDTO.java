5b185c2741fb7c9f4f146a3483f9efa81b7a687c
Shchava <yarosshc@gmail.com>
2019-08-06 23:19:39 +0300

    add getters and setters to UserDTO

--- src/main/java/ua/training/servlet/hospital/entity/dto/UserDTO.java
+++ src/main/java/ua/training/servlet/hospital/entity/dto/UserDTO.java
@@ -5,16 +5,66 @@ import ua.training.servlet.hospital.entity.enums.Roles;
 
 public class UserDTO {
     private String name;
-
     private String surname;
-
     private String patronymic;
-
     private String email;
-
     private String password;
-
     private String confirmPassword;
-
     private Roles role;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+
+    public Roles getRole() {
+        return role;
+    }
+
+    public void setRole(Roles role) {
+        this.role = role;
+    }
 }
