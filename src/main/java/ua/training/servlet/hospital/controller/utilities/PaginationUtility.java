513b162d60dad2fd222a3cabfe3fd10eb22569b1
Shchava <yarosshc@gmail.com>
2019-08-08 01:34:31 +0300

    move utilities to controller package, add records attribute to request

---
+++ src/main/java/ua/training/servlet/hospital/controller/utilities/PaginationUtility.java
@@ -0,0 +1,36 @@
+package ua.training.servlet.hospital.controller.utilities;
+
+import javax.servlet.http.HttpServletRequest;
+
+import static java.util.Objects.nonNull;
+
+public class PaginationUtility {
+    private int page = 1;
+    private int recordsPerPage = 5;
+
+    public void setAttributes(HttpServletRequest request, int rows) {
+        if (nonNull(request.getParameter("page"))) {
+            page = Integer.parseInt(request.getParameter("page"));
+        }
+        if (nonNull(request.getParameter("recordsPerPage"))) {
+            recordsPerPage = Integer.parseInt(request.getParameter("recordsPerPage"));
+        }
+
+        int numberOfPages = (int) Math.ceil(rows * 1.0 / recordsPerPage);
+
+        request.setAttribute("numberOfPages", numberOfPages);
+        request.setAttribute("page", page);
+        request.setAttribute("recordsPerPage", recordsPerPage);
+        request.setAttribute("records", rows);
+    }
+
+    public int getOffset() {
+        return (page - 1) * recordsPerPage;
+    }
+
+    public int getRecordsPerPage() {
+        return recordsPerPage;
+    }
+
+    ;
+}
---
+++ src/test/java/ua/training/servlet/hospital/controller/utilities/PaginationUtilityTest.java
@@ -0,0 +1,51 @@
+package ua.training.servlet.hospital.controller.utilities;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletRequest;
+import javax.servlet.http.HttpServletRequest;
+
+import static org.junit.Assert.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+public class PaginationUtilityTest {
+    @Mock
+    HttpServletRequest request;
+
+    PaginationUtility utility = new PaginationUtility();
+
+    @Before
+    public void setUp(){
+        initMocks(this);
+        given(request.getParameter("page")).willReturn("4");
+        given(request.getParameter("recordsPerPage")).willReturn("5");
+    }
+
+    @Test
+    public void setAttributes() {
+        utility.setAttributes(request,43);
+
+        verify(request,times(1)).setAttribute("numberOfPages",9);
+        verify(request,times(1)).setAttribute("page",4);
+        verify(request,times(1)).setAttribute("recordsPerPage",5);
+        verify(request,times(1)).setAttribute("records",43);
+    }
+
+    @Test
+    public void getOffset() {
+        utility.setAttributes(request,43);
+        assertEquals(15,utility.getOffset());
+    }
+
+    @Test
+    public void getRecordsPerPage() {
+        utility.setAttributes(request,43);
+        assertEquals(5,utility.getRecordsPerPage());
+    }
+}
