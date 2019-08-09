d3d43eb4557aba7c05ae8aba7348fc6b283c0b95
Shchava <yarosshc@gmail.com>
2019-08-09 04:05:57 +0300

    implement ShowPatientDiagnoses command

--- src/main/java/ua/training/servlet/hospital/controller/command/ShowPatient/ShowPatientDiagnoses.java
+++ src/main/java/ua/training/servlet/hospital/controller/command/ShowPatient/ShowPatientDiagnoses.java
@@ -1,15 +1,48 @@
 package ua.training.servlet.hospital.controller.command.ShowPatient;
 
 import ua.training.servlet.hospital.controller.command.Command;
+import ua.training.servlet.hospital.controller.utilities.PaginationUtility;
+import ua.training.servlet.hospital.entity.Diagnosis;
+import ua.training.servlet.hospital.entity.User;
+import ua.training.servlet.hospital.service.ServiceFactory;
+import ua.training.servlet.hospital.service.diagnosis.DiagnosisService;
+import ua.training.servlet.hospital.service.user.UserService;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
+import java.util.NoSuchElementException;
+
+import static ua.training.servlet.hospital.controller.command.utilities.GetUserId.getUserId;
 
 public class ShowPatientDiagnoses implements Command {
+    private ServiceFactory factory = ServiceFactory.getInstance();
+    private UserService userService = factory.getUserService();
+    private DiagnosisService diagnosisService = factory.getDiagnosisService();
+    private PaginationUtility pagination = new PaginationUtility();
+
     @Override
     public String execute(HttpServletRequest request) {
+        long idUser;
+        User patient;
 
+        try {
+            idUser = getUserId(request);
+            patient = userService.getUser(idUser).orElseThrow(NoSuchElementException::new);
+        }catch (NumberFormatException|NoSuchElementException ex){
+            return "/notFoundPage.jsp";
+        }
+
+        long diagnosesCount = diagnosisService.getNumberOfDiagnosesByPatientId(idUser);
+
+        pagination.setAttributes(request,diagnosesCount);
+
+        List<Diagnosis> diagnoses = diagnosisService.findDiagnosesByPatientId(pagination.getPage(),pagination.getRecordsPerPage(),idUser);
+
+        request.setAttribute("patient",patient);
+        request.setAttribute("diagnoses",diagnoses);
 
         return  "/showPatient.jsp";
     }
+
+
 }
---
+++ src/test/java/ua/training/servlet/hospital/controller/command/ShowPatient/ShowPatientDiagnosesTest.java
@@ -0,0 +1,82 @@
+package ua.training.servlet.hospital.controller.command.ShowPatient;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import ua.training.servlet.hospital.controller.utilities.PaginationUtility;
+import ua.training.servlet.hospital.entity.Diagnosis;
+import ua.training.servlet.hospital.entity.User;
+import ua.training.servlet.hospital.service.ServiceFactory;
+import ua.training.servlet.hospital.service.diagnosis.DiagnosisService;
+import ua.training.servlet.hospital.service.user.UserService;
+
+import javax.servlet.http.HttpServletRequest;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.Assert.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.*;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+public class ShowPatientDiagnosesTest {
+    @Mock
+    HttpServletRequest request;
+
+    @Mock
+    List<Diagnosis> foundDiagnoses;
+
+    @Mock
+    ServiceFactory serviceFactory;
+
+    @Mock
+    UserService userService;
+
+    @Mock
+    DiagnosisService diagnosisService;
+
+    @Mock
+    PaginationUtility pagination;
+
+    @InjectMocks
+    ShowPatientDiagnoses showPatientDiagnoses = new ShowPatientDiagnoses();
+
+    @Mock
+    User patient;
+
+    @Mock
+    List<Diagnosis> diagnoses;
+
+    @Before
+    public void setUp() throws Exception {
+        initMocks(this);
+        given(pagination.getRecordsPerPage()).willReturn(5);
+        given(pagination.getPage()).willReturn(2);
+
+        given(request.getRequestURI()).willReturn("/patient/2/");
+        given(userService.getUser(2L)).willReturn(Optional.of(patient));
+        given(diagnosisService.getNumberOfDiagnosesByPatientId(2L)).willReturn(10L);
+        given(diagnosisService.findDiagnosesByPatientId(2,5,2L)).willReturn(diagnoses);
+    }
+
+    @Test
+    public void showPatient(){
+        assertEquals("/showPatient.jsp",showPatientDiagnoses.execute(request));
+
+        verify(request,times(1)).setAttribute("patient",patient);
+        verify(request,times(1)).setAttribute("diagnoses",diagnoses);
+    }
+
+    @Test
+    public void showNotExistingPatient(){
+        given(userService.getUser(2L)).willReturn(Optional.empty());
+
+        assertEquals("/notFoundPage.jsp",showPatientDiagnoses.execute(request));
+
+        verify(request,times(0)).setAttribute("patient",patient);
+        verify(request,times(0)).setAttribute("diagnoses",diagnoses);
+    }
+}
