5b359c15b4a280c5f2ed882edb0584d368ea64a0
Shchava <yarosshc@gmail.com>
2019-08-14 02:38:06 +0300

    add close diagnosis method

--- src/main/java/ua/training/servlet/hospital/dao/DiagnosisDao.java
+++ src/main/java/ua/training/servlet/hospital/dao/DiagnosisDao.java
@@ -2,9 +2,12 @@ package ua.training.servlet.hospital.dao;
 
 import ua.training.servlet.hospital.entity.Diagnosis;
 
+import java.time.LocalDateTime;
 import java.util.List;
+
 
 public interface DiagnosisDao extends GenericDao<Diagnosis> {
     List<Diagnosis> findDiagnosesByPatientId(int start, int count, long patientId);
     long countDiagnosesOfPatient(long patientId);
+    boolean closeDiagnosis(long idDiagnosis, LocalDateTime timeOfClose);
 }
